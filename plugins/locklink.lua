ed70ed9950954515960e9530ca107f25dbec46c9
alireza <alirezakhirandish@gmail.com>
2016-01-10 17:35:07 +0330

    Create locklink.lua

---
+++ plugins/locklink.lua
@@ -0,0 +1,16 @@
+do
+local function run(msg)
+    if not is_mod(msg) and msg.type = 'chat' then
+        --chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+        return 'No links here!'
+    end
+end
+
+return {patterns = {
+    ".com",
+    "http://",
+    "https://",
+    "adf.ly"
+    }, run = run}
+
+end
